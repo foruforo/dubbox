f6ad63d2e6c859c8289b6e679b32d4b38c4e2450
foruforo <foruforo@msn.com>
2016-05-03 11:45:52 +0800

    解决一个容器发布多个provider时可能造成端口冲突。

--- dubbo-common/src/main/java/com/alibaba/dubbo/common/utils/NetUtils.java
+++ dubbo-common/src/main/java/com/alibaba/dubbo/common/utils/NetUtils.java
@@ -74,7 +74,8 @@ public class NetUtils {
     
     public static int getAvailablePort(int port) {
     	if (port <= 0) {
-    		return getAvailablePort();
+//    		return getAvailablePort();
+            port = getAvailablePort();
     	}
     	for(int i = port; i < MAX_PORT; i ++) {
     		ServerSocket ss = null;
